3d4cf253046b7e7a1ff6ec0ad4dc1518cea1c021
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-10-07 21:36:44 +0700

    Update appointmentModel.go

--- model/appointmentModel.go
+++ model/appointmentModel.go
@@ -14,4 +14,5 @@ type Appointment struct {
 	WorkHour  string       `gorm:"type:varchar(100);not null"`
 	TotalPrice  float32 `gorm:"not null"`
 	ProfileID   uint
+	// Transactions   []Transaction `gorm:"foreignKey:AppointmentID"`
 }
